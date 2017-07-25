cefadbf037f2e3cc8ad06fc7c3155e842785ec39
524987246 <524987246@qq.com>
2017-07-25 11:12:34 +0800

    上传文件配置整改

--- src/main/java/com/ztesoft/dispatcher/Application.java
+++ src/main/java/com/ztesoft/dispatcher/Application.java
@@ -2,10 +2,15 @@ package com.ztesoft.dispatcher;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.web.MultipartAutoConfiguration;
 import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.multipart.MultipartResolver;
+import org.springframework.web.multipart.commons.CommonsMultipartResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 /**
@@ -18,6 +23,7 @@ import org.springframework.web.servlet.config.annotation.EnableWebMvc;
  */
 @Controller
 @EnableWebMvc
+@EnableAutoConfiguration(exclude = { MultipartAutoConfiguration.class })
 @ServletComponentScan
 @SpringBootApplication
 public class Application {
@@ -33,4 +39,15 @@ public class Application {
     public String webViewIndex() {
         return "redirect:" + webViewIndex;
     }
+ // 显示声明CommonsMultipartResolver为mutipartResolver
+ 	@Bean(name = "multipartResolver")
+ 	public MultipartResolver multipartResolver() {
+ 		CommonsMultipartResolver resolver = new CommonsMultipartResolver();
+ 		resolver.setDefaultEncoding("UTF-8");
+ 		resolver.setResolveLazily(true);// resolveLazily属性启用是为了推迟文件解析，以在在UploadAction中捕获文件大小异常
+ 		resolver.setMaxInMemorySize(40960);
+ 		resolver.setMaxUploadSize(50 * 1024 * 1024);// 上传文件大小 50M 50*1024*1024
+ 		System.out.println("1111");
+ 		return resolver;
+ 	}
 }
